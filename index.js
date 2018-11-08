f308b7a4ee7eb067b2bb95ba107364350db5104e
Franz Jacanamejoy <franzjacanamejoy@hotmail.com>
2018-11-08 18:10:03 -0500

    cambios de clase

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call'); 
+
+//call.withCallback('Franz',call.sync)
+
+call.withPromise('Franz')
+  .then(name=>console.log(name));
