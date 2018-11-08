b85e0ded0462d15860fd51a9c1a4efc80d487b69
Franz Jacanamejoy <franzjacanamejoy@hotmail.com>
2018-11-08 18:38:43 -0500

    cambios

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call');
+
+// call.greet('Franz Jacanamejoy')
+
+// call.withPromise('Camilo','Jacanamejoy')
+//   .then((n)=>console.log(n))
+
+async function callWithPromise() {
+  const fullName = await call.withPromise('Franz','Jacanamejoy');
+  console.log(fullName)
+}
+
+callWithPromise();
